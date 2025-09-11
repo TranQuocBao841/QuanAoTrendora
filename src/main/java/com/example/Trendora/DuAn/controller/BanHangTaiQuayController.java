abbbe3f6807f3141b931f706cb5c7b209d2c3f6c
Quoc Bao <bao2004tk@gmail.com>
2025-09-11 20:51:43 +0700

    up lại

--- src/main/java/com/example/Trendora/DuAn/controller/BanHangTaiQuayController.java
+++ src/main/java/com/example/Trendora/DuAn/controller/BanHangTaiQuayController.java
@@ -565,7 +565,7 @@ public class BanHangTaiQuayController {
         hoaDon.setKhachHang(khachHang);
         hoaDon.setHinhThucThanhToan(hinhThuc);
         hoaDon.setGiamGia(giamGia);
-        hoaDon.setTrangThaiDonHang(TrangThaiDonHang.DA_HOAN_THANH);
+        hoaDon.setTrangThaiDonHang(TrangThaiDonHang.DA_XAC_NHAN);
 
         // **Set địa chỉ giao hàng**
         hoaDon.setDiaChiGiaoHang(diaChiGiaoHang);
